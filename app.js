a2e531e0a780b79ec6869157784cdb9840d6296f
sspony <19336274+sspony@users.noreply.github.com>
2021-02-27 22:04:23 +0300

    fix console log text

--- app.js
+++ app.js
@@ -4,27 +4,27 @@ const https = require('https');
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  var today = new Date();
-  var options = {
+  let today = new Date();
+  let options = {
     weekday: 'long',
     day: 'numeric',
     month: 'long'
   };
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render('list', { day: day, items: items });
 });
 
 app.post("/", function(req, res) {
   item = req.body.newItem;
-  console.log("Post request received:" + item);
+  console.log("Post request received: '" + item + "'");
   items.push(item);
 
   res.redirect("/");
