7ae647f50939394da9da3ea2b75563d648bbcf18
sspony <19336274+sspony@users.noreply.github.com>
2021-02-27 23:13:03 +0300

    add work list

--- app.js
+++ app.js
@@ -5,28 +5,56 @@ const https = require('https');
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+///////////./////////////////////////// / //////////////////////////////////////
 app.get("/", function(req, res) {
   let day = currentDate();
-  res.render('list', { day: day, items: items });
+  res.render('list', { listTitle: day, items: items });
 });
 
 app.post("/", function(req, res) {
   item = req.body.newItem;
   console.log("Post request received: '" + item + "'");
+
   if (item !== "") {
-    items.push(item);
+    if (req.body.list === "Work") {
+      workItems.push(item);
+      res.redirect("/work");
+    }
+    else {
+      items.push(item);
+      res.redirect("/");
+    }
   }
 
-  res.redirect("/");
 });
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
+////////////////////////////////////////////////////////////////////////////////
 
+
+///////////////////////////////////// WORK /////////////////////////////////////
+app.get("/work", function(req, res) {
+  res.render('list', { listTitle: "Work List", items: workItems });
+});
+
+app.post("/work", function(req, res) {
+  let item = req.body.newItem;
+  if (item !== "") {
+    workItems.push(item);
+  }
+
+  res.redirect("/work");
+});
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////// FUNCTIONS //.////////////////////////////////
 function currentDate() {
   let today = new Date();
   let options = {
@@ -36,3 +64,4 @@ function currentDate() {
   };
   return today.toLocaleDateString("en-US", options);
 }
+////////////////////////////////////////////////////////////////////////////////
