d9c0a22b97cdb0cd8dbc119dd399771b833d41d7
sspony <19336274+sspony@users.noreply.github.com>
2021-02-27 18:27:10 +0300

    add weekend example 2

--- app.js
+++ app.js
@@ -9,21 +9,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
   //res.sendFile(__dirname + "/index.html");
-  res.write(weekend());
+
+  var today = new Date();
+  var currentDay = today.getDay();
+  if (currentDay === 6 || currentDay === 0) {
+    res.write("<h1>Yay it's the weekend!</h1>");
+  } else {
+    res.write("<p>Boo! I have to work!</p>");
+    res.write("<h1>Boo! I have to work!</h1>");
+    res.send();
+  }
 });
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server is running...");
 });
-
-
-function weekend () {
-  var today = new Date();
-  var currentDay = today.getDay();
-  if (currentDay === 6 || currentDay === 0) {
-    return "<h1>Yay it's the weekend!</h1>";
-  }
-  else {
-    return "<h1>Boo! I have to work!</h1>";
-  }
-}
