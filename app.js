a44368825fc9589e93f4aab86970a97a1b17cc3f
sspony <19336274+sspony@users.noreply.github.com>
2021-02-27 18:51:29 +0300

    add ejs and weekend example 4

--- app.js
+++ app.js
@@ -18,7 +18,33 @@ app.get("/", function(req, res) {
   } else {
     day = "Weekday";
   }
-  res.render('list', {day: day});
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("current day error");
+  }
+  res.render('list', { day: day });
 });
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
