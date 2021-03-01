2f52b46423aed7cae692cd189b8e4bb0647a98e5
Pronoia9 <19336274+pronoia9@users.noreply.github.com>
2021-03-02 01:29:00 +0300

    add mongodb for lists (342-346 done)

--- app.js
+++ app.js
@@ -2,61 +2,139 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const https = require('https');
-
-const date = require(__dirname + '/date.js');
+const mongoose = require('mongoose');
 
 const app = express();
 ////////////////////////////////////////////////////////////////////////////////
 
-
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
-///////////./////////////////////////// / //////////////////////////////////////
+///////////////////////////////////  MONGODB  //////////////////////////////////
+const db = "todoDB";
+const url = "mongodb://localhost:27017/" + db;
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Create schema
+const itemsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Item name missing! Please enter some text."]
+  }
+});
+
+const listSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Item name missing! Please enter some text."]
+  },
+  items: [itemsSchema]
+});
+
+// Create model based on schema
+const Item = mongoose.model('item', itemsSchema);
+const List = mongoose.model('list', listSchema);
+////////////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////  /  /////////////////////////////////////
 app.get("/", function(req, res) {
-  res.render('list', { listTitle: date.getDate(), items: items });
+  // get the list of items from the database
+  Item.find({}, function(err, items) {
+    try {
+      // if the database is empty, fill it with default documents
+      if (items.length === 0) {
+        initDefaultList();
+        res.redirect("/");
+      } else {
+        res.render('list', { listTitle: "Today", items: items });
+      }
+    } catch (e) { console.log("** ERROR IN APP.GET('/') **" + e); }
+  });
 });
 
 app.post("/", function(req, res) {
-  item = req.body.newItem;
-  console.log("Post request received: '" + item + "'");
+  try {
+    // get new item from user
+    const item = new Item ({
+      name: req.body.newItem
+    });
 
-  if (item !== "") {
-    if (req.body.list === "Work") {
-      workItems.push(item);
-      res.redirect("/work");
+    // add the new item to the collection
+    item.save().then(() => console.log("Successfully added an item:   '" + item.name + "'"));
+
+    res.redirect("/");
+  } catch (e) { console.log("** ERROR IN APP.POST('/') **" + e); }
+});
+////////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////  DELETE  ///////////////////////////////////
+app.post("/delete", function(req, res) {
+  // get the _id of the item
+  const checkedItem = req.body.checkbox;
+
+  Item.findByIdAndDelete(checkedItem, function(err) {
+    if (err) {
+      console.log("** ERROR IN APP.POST('/DELETE') **" + err);
     }
     else {
-      items.push(item);
+      console.log("Successfully removed an item: '" + checkedItem + "'");
+    }
+    res.redirect("/");
+  });
+});
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////  :/CUSTOMLIST  ////////////////////////////////
+app.get("/:customList", function(req, res) {
+  const customList = req.params.customList;
+
+  //initCustomList(customList);
+  List.findOne({name: customList}, function(err, list) {
+    if (!err) {
+      if (!list) {
+        console.log("List '" + customList + "' does not exist!");
+
+        // Create a new list
+        const list = new List ({
+          name: customList,
+          items: getLoremIpsumListItems()
+        });
+        list.save();
+        res.redirect("/" + customList);
+      }
+      else {
+        console.log("List '" + customList + "' found!");
+
+        // Show an existing list
+        res.render('list', { listTitle: customList, items: list.items });
+      }
+    }
+    else {
+      console.log("** ERROR IN APP.GET('/:CUSTOMLIST') **" + err);
       res.redirect("/");
     }
-  }
+  });
+});
+
+app.post("/:customList", function(req, res) {
+  // let item = req.body.newItem;
+  // if (item !== "") {
+  //   workItems.push(item);
+  // }
+  //
+  // res.redirect("/work");
 });
 ////////////////////////////////////////////////////////////////////////////////
 
 
-///////////////////////////////////// WORK /////////////////////////////////////
-app.get("/work", function(req, res) {
-  res.render('list', { listTitle: "Work List", items: workItems });
-});
-
-app.post("/work", function(req, res) {
-  let item = req.body.newItem;
-  if (item !== "") {
-    workItems.push(item);
-  }
-
-  res.redirect("/work");
-});
-////////////////////////////////////////////////////////////////////////////////
-
-
-///////////////////////////////////// ABOUT ////////////////////////////////////
+////////////////////////////////////  ABOUT  ///////////////////////////////////
 app.get("/about", function(req, res) {
   res.render('about');
 });
@@ -64,3 +142,78 @@ app.get("/about", function(req, res) {
 
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
+
+
+//////////////////////////////////  FUNCTIONS  /////////////////////////////////
+function getStartingListItems() {
+  // Create default documents
+  const item1 = new Item({
+    name: "Welcome to your ToDo List!"
+  });
+  const item2 = new Item({
+    name: "Hit the + button to add a new item."
+  });
+  const item3 = new Item({
+    name: "<-- Hit the checkbox to delete an item."
+  });
+  return [item1, item2, item3];
+}
+
+function getLoremIpsumListItems() {
+  // Create default documents
+  const item1 = new Item({
+    name: "Lorem ipsum dolor sit amet"
+  });
+  const item2 = new Item({
+    name: "Consectetur adipiscing elit"
+  });
+  const item3 = new Item({
+    name: "Duis id erat congue"
+  });
+  return [item1, item2, item3];
+}
+
+function initDefaultList() {
+  // Insert default itemsSchema
+  Item.insertMany(getStartingListItems(), function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfully added default items to the list.");
+    }
+  });
+}
+
+function initCustomList(customName) {
+  const list = new List ({
+    name: customName,
+    items: getLoremIpsumListItems()
+  });
+  list.save();
+
+  // List.findOne({name: customList}, function(err, list) {
+  //   if (!err) {
+  //     if (!list) {
+  //       console.log("List '" + customList + "' does not exist!");
+  //
+  //       // Create a new list
+  //       const list = new List ({
+  //         name: customList,
+  //         items: getLoremIpsumListItems()
+  //       });
+  //       list.save();
+  //     }
+  //     else {
+  //       console.log("List '" + customList + "' found!");
+  //
+  //       // Show an existing list
+  //       res.render('list', { listTitle: customList, items: list.items });
+  //     }
+  //   }
+  //   else {
+  //     console.log("** ERROR IN APP.GET('/:CUSTOMLIST') **" + err);
+  //     res.redirect("/");
+  //   }
+  // });
+}
+////////////////////////////////////////////////////////////////////////////////
